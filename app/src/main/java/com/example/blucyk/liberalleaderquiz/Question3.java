c486fa16da37861762c1cdae7e53212b6861c11f
Bryanna Lucyk <67807118+brylucyk@users.noreply.github.com>
2021-05-16 19:40:27 -0400

    Reconfigured to support onPause and onResume events.
    Signed-off-by: brylucyk <brylucyk@gmail.com>

--- app/src/main/java/com/example/blucyk/liberalleaderquiz/Question3.java
+++ app/src/main/java/com/example/blucyk/liberalleaderquiz/Question3.java
@@ -2,15 +2,20 @@ package com.example.blucyk.liberalleaderquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class Question3 extends AppCompatActivity implements IQuestion {
+
+    final String [] rdoBtnIds = {"bobRaeBtn", "mikeHarrisBtn", "dougFordBtn", "kathleenWynneBtn"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +37,15 @@ public class Question3 extends AppCompatActivity implements IQuestion {
             correctAnswer = true;
         }
 
-        affectResult(correctAnswer);
+        displayResult(correctAnswer);
     }
 
     /**
      * Will notify the user of a correct answer, and change the rightImg and results text
-     * accordingly. On correct answer, will also give the user the ability to close the app.
+     * accordingly. Calls changeBtnPath().
      * @param correctAnswer: true if user's answer was correct, false if not
      */
-    public void affectResult(boolean correctAnswer) {
+    public void displayResult(boolean correctAnswer) {
         TextView resultsText = findViewById(R.id.resultsText);
         ImageView rightImg = findViewById(R.id.rightImg);
 
@@ -48,18 +53,80 @@ public class Question3 extends AppCompatActivity implements IQuestion {
             resultsText.setText(getString(R.string.answer_correct));
             rightImg.setImageResource(R.drawable.circle_kw);
             rightImg.setContentDescription(getString(R.string.kw_imgDesc));
-            Button submitBtn = findViewById(R.id.submitBtn);
+
+            // disable buttons
+            for (String rdoBtnId : rdoBtnIds) {
+                RadioButton radioButton = findViewById(getResources().getIdentifier(rdoBtnId, "id", getPackageName()));
+                radioButton.setEnabled(false);
+            }
+        }
+        else {
+            resultsText.setText(R.string.answer_incorrect);
+        }
+
+        Button submitBtn = findViewById(R.id.submitBtn);
+        changeBtnPath(correctAnswer, submitBtn);
+    }
+
+    /**
+     * Changes the behaviour of the submit button so that the user can proceed.
+     * @param submitBtn: true if user's answer was correct, false if not
+     */
+    public void changeBtnPath(Boolean correctAnswer, Button submitBtn) {
+        if(correctAnswer) {
             submitBtn.setText(getString(R.string.close_btn));
-            submitBtn.setOnClickListener(new View.OnClickListener()
-            {
-                public void onClick(View v)
-                {
+
+            submitBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
                     finish();
                 }
             });
         }
         else {
-            resultsText.setText(R.string.answer_incorrect);
+            submitBtn.setText(getString(R.string.submit_btn));
+
+            submitBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    checkAnswer(findViewById(android.R.id.content));
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        SharedPreferences settings = getSharedPreferences("datapersistance", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+        // find which radio button was checked
+        for (String rdoBtnId : rdoBtnIds) {
+            RadioButton radioButton = findViewById(getResources().getIdentifier(rdoBtnId, "id", getPackageName()));
+            if (radioButton.isChecked()) {
+                editor.putString("q3_btn_checked", rdoBtnId);
+            }
+        }
+
+        editor.apply();
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences settings = getSharedPreferences("datapersistance",Context.MODE_PRIVATE);
+        String btnChecked = settings.getString("q3_btn_checked", null);
+
+        if(btnChecked != null) {
+            RadioGroup rg = findViewById(R.id.questionOptions);
+            rg.check(getResources().getIdentifier(btnChecked, "id", getPackageName()));
+        }
+
+        if(btnChecked != null) {
+            RadioGroup rg = findViewById(R.id.questionOptions);
+            rg.check(getResources().getIdentifier(btnChecked, "id", getPackageName()));
+            checkAnswer(findViewById(android.R.id.content));
         }
     }
 
